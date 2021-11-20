96692064c722b0ef4611f0997ff562833f82ab07
micheclark32 <55514405+micheclark32@users.noreply.github.com>
2021-11-20 12:20:23 -0800

    fixes to week10

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,12 +1,12 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=e85fab00cec86afdaf861396b3d914f1";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=e85fab00cec86afdaf861396b3d914f1";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    document.getElementById('condition').textContent = jsObject.weather[0].description;
-    document.getElementById('current-temp').textContent = jsObject.main.temp.toFixed(0);
-    document.getElementById('humidity').textContent = jsObject.main.humidity;
-    document.getElementById('wind-speed').textContent = jsObject.wind.speed.toFixed(0);
+    document.getElementById('description').textContent = jsObject.weather[0].description;
+    document.getElementById('current-temp').textContent = jsObject.main.temp.toFixed(0) + "°F";
+    document.getElementById('humidity').textContent = jsObject.main.humidity + "%";
+    document.getElementById('wind-speed').textContent = jsObject.wind.speed.toFixed(0) + "MPH";
     
     const temp = parseFloat(document.querySelector('#current-temp').textContent);
     const wind = parseFloat(document.querySelector('#wind-speed').textContent);
@@ -27,7 +27,7 @@ fetch(apiURL)
     return f.toFixed(0);
     }
 
-    document.querySelector('.windchill').innerHTML = `${wchill}°F`;
+    document.querySelector('windchill').innerHTML = `${wchill}°F`;
   
   
   
