b0ef76ead1b7305023724d87ac7c3b963abd0ace
micheclark32 <55514405+micheclark32@users.noreply.github.com>
2021-11-22 22:39:29 -0800

    fix for week10 js

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -10,24 +10,19 @@ fetch(apiURL)
     
     const temp = parseFloat(document.querySelector('#current-temp').textContent);
     const wind = parseFloat(document.querySelector('#wind-speed').textContent);
-    let wchill;
 
-    if (temp <= 50 && wind > 3) {
-    wchill = windChill(temp, wind);
-
+      if (temp <= 50 && wind > 3) {
+      const windchill =
+        35.74 +
+        0.6215 * temp -
+        35.75 * Math.pow(wind, 0.16) +
+        0.4275 * temp * Math.pow(wind, 0.16);
+      document.querySelector("#wind-chill").textContent = windchill.toFixed(1);
+      } else {
+      const windChillDescriptor = "N/A";
+      document.querySelector("#wind-chill").textContent =
+        windChillDescriptor;
     }
-    else {
-     wchill = `N/A`;
-    } 
-
-    function windChill(temp, wind){
-    let f = 35.74 + (temp * 0.6215) 
-    - (35.75 * Math.pow(wind, 0.16)) 
-    + (0.4275 * temp * Math.pow(wind, 0.16));
-    return f.toFixed(0);
-    }
-
-    document.querySelector('windchill').innerHTML = `${wchill}°F`;
   
   
   
