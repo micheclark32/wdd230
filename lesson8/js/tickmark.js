d56426d813db931a864d07a34e44afdba36d25fd
micheclark32 <55514405+micheclark32@users.noreply.github.com>
2021-11-22 23:18:04 -0800

    fixed storm page option

--- lesson8/js/tickmark.js
+++ lesson8/js/tickmark.js
@@ -9,3 +9,13 @@ function selectResponse() {
 	s.textContent = sel.value;
 	
 }
+
+/***Removing Select Region from Value***/
+
+var selectElement = document.getElementById("Region");
+
+for (var i = selectElement.length - 1; i >= 0; i--){
+  if (selectElement.options[i].value == 'A') {
+    selectElement.remove(i);
+  }
+}
