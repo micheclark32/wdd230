82f5b6f2459a1c42f6eddae830246b2d6edf4f4a
micheclark32 <55514405+micheclark32@users.noreply.github.com>
2021-12-10 23:55:14 -0800

    final project first push

---
+++ finalproject/js/forcast.js
@@ -0,0 +1,21 @@
+const apiURL2 = "https://api.openweathermap.org/data/2.5/forecast?id=5803990&units=imperial&appid=e85fab00cec86afdaf861396b3d914f1";
+fetch(apiURL2)
+.then((response) => response.json())
+.then((jsObject) => {
+  
+
+  const dayOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+  let list = jsObject.list.filter(x => x.dt_txt.includes("18:00:00"));
+  
+  for (let day = 0; day <= 4; day ++) {
+      let date = new Date(list[day].dt_txt);
+      document.getElementById(`p-day${day+1}`).innerHTML = dayOfWeek[date.getDay()];
+      document.getElementById(`p-temp${day+1}`).innerHTML = list[day].main.temp + "°F";
+
+      const imgAlt = list[day].weather[0].description;
+      const imageSrc = 'https://openweathermap.org/img/wn/' + list[day].weather[0].icon + '@2x.png';
+      document.getElementById(`p-icon${day+1}`).setAttribute('src', imageSrc);
+      document.getElementById(`p-icon${day+1}`).setAttribute('alt', imgAlt);
+  }
+
+  });
